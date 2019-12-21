01df2b5977eaa8f202827fd41057723d1b72af1b
vinceproj <vince.project.design@gmail.com>
2019-12-21 15:43:59 -0800

    V1

--- script/script.js
+++ script/script.js
@@ -67,6 +67,23 @@ const rightArr = () => {
     })
 }
 
+function openExp(evt, expName) {
+    var i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tabLink");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(expName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 const navApp = () => {
     navSlide();
     leftArr();
